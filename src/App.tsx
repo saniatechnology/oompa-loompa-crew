95cd93301942e19e0f4136bb75b6e95becd74e73
Sania García <58360969+saniatechnology@users.noreply.github.com>
2025-07-02 13:48:08 +0200

    set up react app skeleton from vite template

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Oompa Loompa's Crew</a>
+      </header>
+      <p className="read-the-docs">[Homepage]</p>
+    </div>
+  );
+}
+
+export default App;
