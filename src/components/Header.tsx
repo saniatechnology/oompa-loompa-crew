8f58670a0a55b21a2130d1c67615a7e964129b1a
Sania García <58360969+saniatechnology@users.noreply.github.com>
2025-07-06 11:07:13 +0200

    style header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,12 @@ import "./Header.css";
 
 const Header: React.FC = () => (
   <header className="header">
-    <img className="header__logo" src="https://s3.eu-central-1.amazonaws.com/napptilus/level-test/imgs/logo-umpa-loompa.png" alt="Logo" />
-    <Link className="header__title" to="/">
-      Oompa Loompa's Crew
-    </Link>
+    <div className="header-content">
+      <img className="header__logo" src="https://s3.eu-central-1.amazonaws.com/napptilus/level-test/imgs/logo-umpa-loompa.png" alt="Logo" />
+      <Link className="header__title" to="/">
+        Oompa Loompa's Crew
+      </Link>
+    </div>
   </header>
 );
 
