39f9f37acec6466bb3aaf2c19c26d8250e18872f
Sania García <58360969+saniatechnology@users.noreply.github.com>
2025-07-08 16:39:50 +0200

    add accessibility features to search input

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -8,7 +8,10 @@ interface SearchBarProps {
 
 const SearchBar: React.FC<SearchBarProps> = ({ value, onChange }) => (
   <div className="search-bar">
-    <input type="text" placeholder="Search" value={value} onChange={onChange} className="search-input" />
+    <label htmlFor="search-input" className="hidden-label">
+      Search
+    </label>
+    <input type="text" placeholder="Search" value={value} onChange={onChange} className="search-input" aria-label="Search workers" id="search-input" />
     <span className="search-divider">|</span>
     <img className="search-icon" src="https://s3.eu-central-1.amazonaws.com/napptilus/level-test/imgs/ic_search.png" alt="search" />
   </div>
